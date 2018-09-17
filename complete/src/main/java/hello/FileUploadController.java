09aa53ab8505c54685e296089dc2162d6fe5d5be
Paul Kelly <pkelw@allstate.com>
2018-09-17 12:26:38 +0100

    getting into loop with spring boot 1.5.14

--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -12,74 +12,19 @@ import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.util.Streams;
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import hello.storage.StorageFileNotFoundException;
-import hello.storage.StorageService;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FileUploadController {
 
-    private final StorageService storageService;
-
-    @Autowired
-    public FileUploadController(StorageService storageService) {
-        this.storageService = storageService;
-    }
-
-    @Autowired
-    private StoreService storeSvc;
-
-    @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
-
-        model.addAttribute("files", storageService.loadAll().map(
-                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                        "serveFile", path.getFileName().toString()).build().toString())
-                .collect(Collectors.toList()));
-
-        return "uploadForm";
-    }
-
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
 
     @PostMapping("/")
-//    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-//            RedirectAttributes redirectAttributes) {
-//
-//        storageService.store(file);
-//        redirectAttributes.addFlashAttribute("message",
-//                "You successfully uploaded " + file.getOriginalFilename() + "!");
-//
-//        return "redirect:/";
-//    }
-
-
-
     public String upload(HttpServletRequest request) {
 
         System.out.println("Inside Upload -" + LocalDateTime.now() );
@@ -110,14 +55,6 @@ public class FileUploadController {
                     // try and write the file via stream
                     System.out.println("File field " + name + " with file name " + item.getName() + " detected." + "about to do a store"  + LocalDateTime.now());
 
-                    StoreRequest sreq = StoreRequest.builder().brandName("Allstate").claimNumber("000179132568")
-                            .docTypeCode("315").referenceId("012345678901234567890123456789024797").systemName("Test Source System")
-                            .origFileName(item.getName())
-                            .build();
-
-                    StoreResponse sresp =  storeSvc.store(sreq, "f861eda6-cc39-4504-bbb2-dbe16e7d3769", "vromb", stream);
-                    System.out.println("Al fresco id id " + sresp.getAlfrescoId() + " finish time " +  LocalDateTime.now() );
-
                     stream.close();
 
                 }
@@ -131,13 +68,6 @@ public class FileUploadController {
             e.printStackTrace();
         }
         return "redirect:/";
-    }
-
-
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
     }
 
 }
